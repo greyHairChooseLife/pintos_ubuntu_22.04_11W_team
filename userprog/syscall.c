af04705c9f6b909d712798d25c7bfa7344d5fdda
tomk1002 <tkwon999@gmail.com>
2025-11-21 17:34:33 +0000

    fix: error was due to filesys.h not included

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -8,6 +8,7 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 #include "threads/synch.h"
+#include "filesys/filesys.h" /* filesys_open 등을 위해 필요할 수 있습니다 */
 
 void syscall_entry(void);
 void syscall_handler(struct intr_frame*);
@@ -20,6 +21,8 @@ static void exit(int status);
 static int create(char* file_name, int initial_size);
 static int write(int fd, const void* buffer, unsigned size);
 static void check_valid_ptr(int count, ...);
+static void check_valid_zone(void* ptr, size_t size);
+static void check_valid_string(char* ptr); // for read
 static int open(const char* file);
 static void close(int fd);
 static int read(int fd, void* buffer, unsigned size);
@@ -174,10 +177,39 @@ static void check_valid_ptr(int count, ...)
     va_end(ptr_ap);
 }
 
+void check_valid_zone(void* ptr, size_t size)
+{
+    if (size == 0)
+        return;
+
+    char* start_addr = (char*)ptr;
+    char* end_addr = start_addr + size - 1; // last valid address
+
+    check_valid_ptr(1, start_addr);
+    check_valid_ptr(1, end_addr);
+
+    char* page_start = (char*)pg_round_up(start_addr);
+
+    while (page_start <= end_addr) {
+        check_valid_ptr(1, page_start);
+        page_start += PGSIZE;
+    }
+}
+
+static void check_valid_string(char* ptr)
+{
+    while (true) {
+        check_valid_ptr(1, ptr);
+        if (*ptr == '\0') {
+            break;
+        }
+        ptr++;
+    }
+}
 
 static int open(const char* file)
 {
-    check_valid_ptr(1,file);
+    check_valid_string(file);
     lock_acquire(&lock); // lock before opening file
 
     struct file* open_file = filesys_open(file);
@@ -191,22 +223,20 @@ static int open(const char* file)
     struct thread* t = thread_current();
     struct file_descriptor* fd = malloc(sizeof(struct file_descriptor));
 
-    fd->fd_file = file;
+    fd->fd_file = open_file;
     fd->fd_val = t->min_fd;
     t->min_fd++;
 
     list_push_back(&t->files_opened, &fd->fd_elem);
     lock_release(&lock); // release lock after fd is pushed in
-
-    return fd->fd_val; // return fd_val
+    return fd->fd_val;   // return fd_val
 }
 
 static void close(int fd)
 {
-    struct file_descriptor* real_fd = find_fd(fd);
     struct thread* t = thread_current();
-
     lock_acquire(&lock); // acquire lock before handling file
+    struct file_descriptor* real_fd = find_fd(fd);
 
     if (real_fd == NULL) {
         lock_release(&lock);
@@ -226,46 +256,48 @@ static void close(int fd)
 
 static int read(int fd, void* buffer, unsigned size)
 {
-
-    check_valid_ptr(1,buffer);
-
-    lock_acquire(&lock); // acquire lock before handling file // but since read can be accessed by multiple processes,
-                         // maybe different lock?
-
-    if (fd == 0) { // read from keyboard(STDOUT)
-        lock_release(&lock);
-        return input_getc();
+    check_valid_zone(buffer, size);
+    // fd 0 reads from keyboard(STDOUT)
+    if (fd == 0) {
+        uint8_t* buf = (uint8_t*)buffer; // cast buffer ONCE
+        for (int i = 0; i < size; i++) {
+            buf[i] = input_getc();
+            if (buf[i] == '\n')
+                break;
+        }
+        return size;
     }
 
+    lock_acquire(&lock);
+    // if fd not 0
     struct file_descriptor* real_fd = find_fd(fd);
-
+    // if invalid file descriptor pointer
     if (real_fd == NULL) {
         lock_release(&lock);
-        return;
+        return -1;
     }
-
+    // if not invalid, get file
     struct file* file = real_fd->fd_file;
+    // read from file
+    int32_t bytes_read = file_read(file, buffer, size);
 
-    int32_t bytes_read =  file_read(file, buffer, size);
-
-    lock_release(&lock); // also to release lock
+    lock_release(&lock);
     return bytes_read;
 }
 
 static int32_t filesize(int fd)
 {
-    struct file_descriptor* real_fd = find_fd(fd);
-
     lock_acquire(&lock); // acquire lock before handling file
+    struct file_descriptor* real_fd = find_fd(fd);
 
     if (real_fd == NULL) {
         lock_release(&lock);
-        return;
+        return -1;
     }
-
-    return file_length(real_fd->fd_file);
-
+    
+    int32_t size = file_length(real_fd->fd_file);
     lock_release(&lock);
+    return size;
 }
 
 // helper function: returns file_descriptor if fd is given.
@@ -280,10 +312,10 @@ static struct file_descriptor* find_fd(int fd)
     struct list_elem* e = NULL;
     // traverse through files_opened of current thread to find matching fd_val
     for (e = list_begin(&t->files_opened); e != list_end(&t->files_opened); e = list_next(e)) {
-        struct file_descriptor* fd = list_entry(e, struct file_descriptor, fd_elem);
+        struct file_descriptor* real_fd = list_entry(e, struct file_descriptor, fd_elem);
 
-        if (fd->fd_val == fd)
-            return fd; // return pointer of file_descriptor found
+        if (real_fd->fd_val == fd)
+            return real_fd; // return pointer of file_descriptor found
     }
     return NULL; // failed to find file descriptor
 }
