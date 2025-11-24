f8d86aa1221b0bb0bcbd46d451edc6d072a83a4b
tomk1002 <tkwon999@gmail.com>
2025-11-24 05:07:53 +0000

    feat: open and close systemcall, find_fd() helper function
    thread.h
    - added files_opened list member to thread struct
    - added file_descriptor struct composed of
      - fd_val - actual value of fd
      - fd_elem - for keeping track of in list files_opened
      - fd_file - to keep track of file of fd
    
    thread.c
    - initialized files_opened in init_thread()
    
    syscall.c
    - syscall_handler ==> switch case for open and close added
    - open() ==> gives a thread file descriptor of minimum value
    - close() ==> removes file descriptor from thread's files_opened list
    - find_fd() ==> returns file_descriptor struct from files_opened list that matches given fd value

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -21,11 +21,8 @@ static void exit(int status);
 static int create(char* file_name, int initial_size);
 static int write(int fd, const void* buffer, unsigned size);
 static void check_valid_ptr(int count, ...);
-static void check_valid_zone(void* ptr, size_t size);
-static void check_valid_string(char* ptr); // for read
 static int open(const char* file);
 static void close(int fd);
-static int read(int fd, void* buffer, unsigned size);
 static int32_t filesize(int fd);
 static struct file_descriptor* find_fd(int fd);
 
@@ -85,12 +82,6 @@ void syscall_handler(struct intr_frame* f UNUSED)
     case SYS_CLOSE:
         close(arg1);
         break;
-    case SYS_FILESIZE:
-        f->R.rax = filesize(arg1);
-        break;
-    case SYS_READ:
-        f->R.rax = read(arg1, arg2, arg3);
-        break;
     default:
         thread_exit();
     }
@@ -116,40 +107,24 @@ static int create(char* file_name, int initial_size)
 
 static int write(int fd, const void* buffer, unsigned size)
 {
-    check_valid_zone(buffer, size); // Check buffer validity
+    check_valid_ptr(1, buffer);
 
     lock_acquire(&lock); // race condition 방지
-    if (fd == 0) {
-        lock_release(&lock);
-        return -1;
-    }
-    if (fd == 1) {
-        if (size <= MAX_CHUNK) {
-            putbuf(buffer, size);
-            lock_release(&lock);
-            return size;
-        } else { // 256 이상은 분할 출력
-            size_t offset = 0;
-            while (offset < size) {
-                size_t chunk_size = size - offset < MAX_CHUNK ? size - offset : MAX_CHUNK;
-                putbuf(buffer + offset, chunk_size);
-                offset += chunk_size;
-            }
-            lock_release(&lock);
-            return offset;
+    char* buf = (char*)buffer;
+
+    if (size <= MAX_CHUNK) {
+        putbuf(buf, size);
+    } else { // 256 이상은 분할 출력
+        size_t offset = 0;
+        while (offset < size) {
+            size_t chunk_size = size - offset < MAX_CHUNK ? size - offset : MAX_CHUNK;
+            putbuf(buf + offset, chunk_size);
+            offset += chunk_size;
         }
     }
-    struct file_descriptor* real_fd = find_fd(fd);
-    // don't use check_valid_ptr() here because real_fd is in kernel heap
-    // malloc is used in syscall.c whilst kernel mode
-    if (real_fd == NULL) {
-        lock_release(&lock);
-        exit(-1);
-    }
-
-    size_t ofs = file_write(real_fd->fd_file, buffer, size);
     lock_release(&lock);
-    return ofs;
+
+    return size;
 }
 
 /**
@@ -177,7 +152,7 @@ static void check_valid_ptr(int count, ...)
             exit(-1);
         }
 
-        // check user segment
+        // Check user segment
         if (ptr < CODE_SEGMENT || ptr >= USER_STACK) {
             va_end(ptr_ap);
             exit(-1);
@@ -193,39 +168,9 @@ static void check_valid_ptr(int count, ...)
     va_end(ptr_ap);
 }
 
-void check_valid_zone(void* ptr, size_t size)
-{
-    if (size == 0)
-        return;
-
-    char* start_addr = (char*)ptr;
-    char* end_addr = start_addr + size - 1; // last valid address
-
-    check_valid_ptr(1, start_addr);
-    check_valid_ptr(1, end_addr);
-
-    char* page_start = (char*)pg_round_up(start_addr);
-
-    while (page_start <= end_addr) {
-        check_valid_ptr(1, page_start);
-        page_start += PGSIZE;
-    }
-}
-
-static void check_valid_string(char* ptr)
-{
-    while (true) {
-        check_valid_ptr(1, ptr);
-        if (*ptr == '\0') {
-            break;
-        }
-        ptr++;
-    }
-}
-
 static int open(const char* file)
 {
-    check_valid_string(file);
+    check_valid_ptr(1, file);
     lock_acquire(&lock); // lock before opening file
 
     struct file* open_file = filesys_open(file);
@@ -239,13 +184,18 @@ static int open(const char* file)
     struct thread* t = thread_current();
     struct file_descriptor* fd = malloc(sizeof(struct file_descriptor));
 
+    int fd_candidate = 2; // if file is closed and releases low fd, lowest fd can be given
+    while (find_fd(fd_candidate) != NULL) {
+        fd_candidate++;
+    }
+
+    fd->fd_val = fd_candidate;
     fd->fd_file = open_file;
-    fd->fd_val = t->min_fd;
-    t->min_fd++;
 
     list_push_back(&t->files_opened, &fd->fd_elem);
-    lock_release(&lock); // release lock after fd is pushed in
-    return fd->fd_val;   // return fd_val
+    lock_release(&lock);
+
+    return fd->fd_val;
 }
 
 static void close(int fd)
@@ -259,61 +209,11 @@ static void close(int fd)
         return;
     }
 
-    if (real_fd->fd_val <
-        t->min_fd) //  update min_fd of thread so that it can give lowest available fd for next file opened
-        t->min_fd = real_fd->fd_val;
-
     file_close(real_fd->fd_file);
     list_remove(&real_fd->fd_elem); // don't forget to remove from list
     free(real_fd);                  // also to free the file descriptor memory allocated by malloc when opened
     lock_release(&lock);            // also to release lock
     return;
-}
-
-static int read(int fd, void* buffer, unsigned size)
-{
-    check_valid_zone(buffer, size);
-    // fd 0 reads from keyboard(STDOUT)
-    if (fd == 0) {
-        uint8_t* buf = (uint8_t*)buffer; // cast buffer ONCE
-        for (int i = 0; i < size; i++) {
-            buf[i] = input_getc();
-            if (buf[i] == '\n')
-                break;
-        }
-        return size;
-    }
-
-    lock_acquire(&lock);
-    // if fd not 0
-    struct file_descriptor* real_fd = find_fd(fd);
-    // if invalid file descriptor pointer
-    if (real_fd == NULL) {
-        lock_release(&lock);
-        return -1;
-    }
-    // if not invalid, get file
-    struct file* file = real_fd->fd_file;
-    // read from file
-    int32_t bytes_read = file_read(file, buffer, size);
-
-    lock_release(&lock);
-    return bytes_read;
-}
-
-static int32_t filesize(int fd)
-{
-    lock_acquire(&lock); // acquire lock before handling file
-    struct file_descriptor* real_fd = find_fd(fd);
-
-    if (real_fd == NULL) {
-        lock_release(&lock);
-        return -1;
-    }
-
-    int32_t size = file_length(real_fd->fd_file);
-    lock_release(&lock);
-    return size;
 }
 
 // helper function: returns file_descriptor if fd is given.
